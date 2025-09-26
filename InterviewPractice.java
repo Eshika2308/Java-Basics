a69d8dfe8023c17ec6c2ab7449d0a281481fadca
Eshika2308 <eshika2308@gmail.com>
2025-09-26 18:35:25 +0530

    Day01 repo setup

---
+++ InterviewPractice.java
@@ -0,0 +1,9 @@
+import OOPS.Fizzbuzz;
+
+public class InterviewPractice {
+    public static void main(String args[]){
+        Fizzbuzz fb = new Fizzbuzz();
+        fb.Hello_world();
+    }
+}
+
---
+++ OOPS/Fizzbuzz.java
@@ -0,0 +1,7 @@
+package OOPS;
+
+public class Fizzbuzz {
+    public void Hello_world(){
+        System.out.println("Hello All!");
+    }
+}
